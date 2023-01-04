0042122832a4921f94928db7ebbfe2483d3371e0
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-01-03 21:36:51 -0600

    feat - Camera movement between rooms

---
+++ Assets/Scripts/Managers/Camera/CameraManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public GameObject virtualCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCamera.SetActive(false);
+        }
+    }
+}
